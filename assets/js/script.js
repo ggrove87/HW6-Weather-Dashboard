a00085d915ee3a65b792f7eac5b24b0a85544fa6
ggrove87 <gwg1387@gmail.com>
2022-04-12 08:03:45 -0500

    Added functionality for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let apiKey = "ed6c43b0f4986d5644abce6786e4511f";
 let searchBtn = document.querySelector("#searchBtn");
 let selectedCity = document.querySelector("#cityInput");
+let cityName = document.querySelector("#cityName");
+let currentTemp = document.querySelector("#tempurature");
+let currentWind = document.querySelector("#windSpeed");
+let currentHumidity = document.querySelector("#humidity");
+let ultraVioletIndex = document.querySelector("#ultraVioletIndex");
 let fiveDayDate = document.querySelectorAll(".card-title")
 let fiveDayTemp = document.querySelectorAll(".card-temp");
 let fiveDayWind = document.querySelectorAll(".card-wind");
@@ -18,10 +23,9 @@ function pullWeatherData(city) {
     })
     .then((response) => response.json())
     .then((cityData) => {
-
+        localStorage.setItem(cityData.city.id, selectedCity)
         displayWeatherData(cityData.list)
-        console.log(cityData.list);
-        console.log(cityData.list[4].main.temp);
+        console.log(cityData.city.id);
     });
 }
 
@@ -33,23 +37,41 @@ function displayWeatherData(city){
     let fiveDayHumidityEl = [];
 
     for (let i = 4; i < 37; i+=8) {
-        
+        fiveDayDateEl[j] = city[i].dt_txt;
         fiveDayTempEl[j] = city[i].main.temp;
         fiveDayWindEl[j] = city[i].wind.speed;
         fiveDayHumidityEl[j] = city[i].main.humidity;
+        fiveDayDate[j].innerText = fiveDayDateEl[j];
         fiveDayTemp[j].innerText = "Temp: " +fiveDayTempEl[j];
-        fiveDayWind[j].innerText = "Wind: "+fiveDayWindEl[j];
+        fiveDayWind[j].innerText = "Wind: "+fiveDayWindEl[j]+" mph";
         fiveDayHumidity[j].innerText = "Humidity: "+fiveDayHumidityEl[j];
-        console.log(city[i].wind);
         j++
-        console.log(j);
     }
-    console.log(fiveDayTemp);
-    console.log(fiveDayTempEl);
+}
+
+function displayCurrentWeather(city) {
+    fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((geoData) => {
+          return fetch(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=imperial&appid=${apiKey}`
+          );
+        })
+        .then((response) => response.json())
+        .then((cityData) => {
+            cityName.innerText = cityData.name;
+            currentTemp.innerText = "Temp: " +cityData.main.temp;
+            currentWind.innerText = "Wind: "+cityData.wind.speed+" mph";
+            currentHumidity.innerText = "Humidity: "+cityData.main.humidity;
+            console.log(cityData);
+        });
 }
 
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
   let currentCity = selectedCity.value;
   pullWeatherData(currentCity);
+  displayCurrentWeather(currentCity);
 });
